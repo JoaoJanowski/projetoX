9860eef3db9447c18a822d0d9238f734a6bd7f2c
gustavodap <72631936+uGustee@users.noreply.github.com>
2024-02-23 12:14:11 -0300

    Update menu2.js
    Carrossel de imagens

--- menu2.js
+++ menu2.js
@@ -10,3 +10,24 @@ function selectLink(){
 menuItem.forEach((item)=>
 	item.addEventListener('click', selectLink)
 )
+
+let currentImageIndex = 0;
+const images = ["image1.jpg", "image2.jpg", "image3.jpg"]; // Adicione mais imagens conforme necessário
+
+const carouselImg = document.getElementById('carouselImg');
+
+function showImage(index) {
+    carouselImg.src = images[index];
+}
+
+function nextImage() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    showImage(currentImageIndex);
+}
+
+function prevImage() {
+    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+    showImage(currentImageIndex);
+}
+
+showImage(currentImageIndex);
