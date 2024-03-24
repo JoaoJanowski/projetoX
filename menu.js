4bfb7b537e7a15b655f7d9cfb1991702f8c0d303
gustavodap <72631936+uGustee@users.noreply.github.com>
2024-03-24 20:41:08 -0300

    Delete menu.js

--- menu.js
+++
@@ -1,6 +0,0 @@
-var btnExp = document.querySelector('#btn-exp')
-var barSide = document.querySelector('.side-bar')
-
-btnExp.addEventListener('click', function(){
-	barSide.classList.toggle('expansão')
-})
