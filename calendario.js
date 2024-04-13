66303317acd2d832d317f9121249dc9d82807d8e
sofysegovia <159036032+sofysegovia@users.noreply.github.com>
2024-04-12 21:05:25 -0300

    Update calendario.js

--- calendario.js
+++ calendario.js
@@ -3,17 +3,17 @@ document.addEventListener("DOMContentLoaded", function() {
   
   // Array de eventos de transmissão
   var events = [
-    { date: "2024-04-15", eventName: "GP da Fórmula 1" },
-    { date: "2024-04-20", eventName: "Jogo do Brasileirão" },
-    { date: "2024-04-20", eventName: "Jogo do Brasileirão" },
-    { date: "2024-04-20", eventName: "Jogo do Brasileirão" },
-    { date: "2024-04-20", eventName: "Jogo do Brasileirão" },
-    { date: "2024-04-20", eventName: "Jogo do Brasileirão" },
-    { date: "2024-04-20", eventName: "Jogo do Brasileirão" },
-    { date: "2024-04-20", eventName: "Jogo do Brasileirão" },
-    { date: "2024-04-20", eventName: "Jogo do Brasileirão" },
-    { date: "2024-04-20", eventName: "Jogo do Brasileirão" },
-    { date: "2024-04-20", eventName: "Jogo do Brasileirão" },
+    { date: "Sex., 12 de Abr., 21:00", eventName: "NBA - Memphis Grizzlies x Los Angeles Lakers" },
+    { date: "Sáb., 13 de Abr., 18:30", eventName: "1ª Rodada Brasileirão Série A - Criciúma x Juventude" },
+    { date: "Sáb., 13 de Abr., 18:30", eventName: "1ª Rodada Brasileirão Série A - Internacional x Bahia" },
+    { date: "Sáb., 13 de Abr., 08:30", eventName: "Masters de Monte Carlo - Stefanos Tsitsipas x Jannik Sinner" },
+    { date: "Dom., 14 de Abr., 14:40", eventName: "MLB - New York Mets x Kansas City Royals" },
+    { date: "Dom., 17 de Abr., 16:00", eventName: "Champions League - Manchester City x Real Madrid" },
+    { date: "Dom., 17 de Abr., 16:00", eventName: "Champions League - Bayern Munchen x Arsenal" },
+    { date: "Dom., 21 de Abr., 04:00", eventName: "GP da Fórmula 1 - Xangaí" },
+    { date: "Sáb., 27 de Abr., 20:00", eventName: "UFC Fight Night / Peso Mosca - Matheus Nicolau x Alex Perez" },
+    { date: "Dom., 5 De Mai., 17:00", eventName: "GP da Fórmula 1 - Miami" },
+
     // Adicione mais eventos conforme necessário
   ];
 
